964c6ce935fac2318de1cc36cdf65d77ed59b275
Anthony Buonomo <arb246@georgetown.edu>
2024-07-24 11:21:06 -0400

    Update download_script.py
    correct the solar cycle 23 start and end times

--- papers/2023A&A...675A.102K/download_script.py
+++ papers/2023A&A...675A.102K/download_script.py
@@ -5,8 +5,8 @@ from sunpy.net import Fido, attrs as a
 import datetime
 
 # Define the time ranges for the observations
-time_range_cycle_23 = (datetime.datetime(1996, 1, 1), datetime.datetime(2008, 12, 31))  # Example time range
-time_range_cycle_24 = (datetime.datetime(2009, 1, 1), datetime.datetime(2019, 12, 31))  # Example time range
+time_range_cycle_23 = (datetime.datetime(1996, 8, 1), datetime.datetime(2001, 11, 30))  # Example time range
+time_range_cycle_24 = (datetime.datetime(2008, 12, 1), datetime.datetime(2014, 4, 31))  # Example time range
 
 # WARNING: The time ranges defined above may be too broad for the VSO to return results.
 # Consider using a smaller example time range for testing, such as:
